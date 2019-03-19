c3306ded29f341eec935ebd8ea83d96bcf1e24b2
Medhir Bhargava <medhirbhargava@gmail.com>
2019-03-18 23:51:21 -0700

    FusionAuthClient -- Rough Draft

--- client.go
+++ client.go
@@ -8,18 +8,13 @@ import (
 	"net/url"
 )
 
-// FusionAuthClient describes the Go Client for interacting with FusionAuth's RESTful API
-type FusionAuthClient struct {
-	BaseURL    *url.URL
-	APIKey     string
-	httpClient *http.Client
+// URIWithSegment returns a string with a "/" delimeter between the uri and segment
+func URIWithSegment(uri, segment string) string {
+	return uri + "/" + segment
 }
 
-func uriWithSegment(uri, segment string) string {
-	return uri + segment
-}
-
-func (c *FusionAuthClient) newRequest(method, path string, body interface{}) (*http.Request, error) {
+// NewRequest creates a new request for the FusionAuth API call
+func (c *FusionAuthClient) NewRequest(method, endpoint string, body interface{}) (*http.Request, error) {
 	rel := &url.URL{Path: path}
 	u := c.BaseURL.ResolveReference(rel)
 
@@ -36,17 +31,18 @@ func (c *FusionAuthClient) newRequest(method, path string, body interface{}) (*h
 	if err != nil {
 		return nil, err
 	}
-	if body != nil {
-		req.Header.Set("Content-Type", "application/json")
-	}
+	// if body != nil {
+	// 	req.Header.Set("Content-Type", "application/json")
+	// }
 
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Authorization", c.APIKey)
+	// req.Header.Set("Accept", "application/json")
+	// req.Header.Set("Authorization", c.APIKey)
 
 	return req, nil
 }
 
-func (c *FusionAuthClient) do(req *http.Request, v interface{}) (*http.Response, error) {
+// Do makes the request to the FusionAuth API endpoint and decodes the response
+func (c *FusionAuthClient) Do(req *http.Request, v interface{}) (*http.Response, error) {
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, err
@@ -54,21 +50,5 @@ func (c *FusionAuthClient) do(req *http.Request, v interface{}) (*http.Response,
 	defer resp.Body.Close()
 
 	err = json.NewDecoder(resp.Body).Decode(v)
-	return resp, err
-}
-
-// RetrieveUser retrieves the user for the given userID
-func (c *FusionAuthClient) RetrieveUser(userID string) (interface{}, error) {
-	req, err := c.newRequest(http.MethodGet, uriWithSegment("api/user", userID), nil)
-	var resp interface{}
-	_, err = c.do(req, &resp)
-	return resp, err
-}
-
-// CreateUser creates a user. You can optionally specify an ID for the user, if not provided one will be generated.
-func (c *FusionAuthClient) CreateUser(userID string, request interface{}) (interface{}, error) {
-	req, err := c.newRequest(http.MethodPost, uriWithSegment("api/user", userID), request)
-	var resp interface{}
-	_, err = c.do(req, &resp)
 	return resp, err
 }
