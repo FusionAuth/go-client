3a548b3c80c0c9484f4ba26bd2b8594f12c519af
Philip Peters <ppeters@aadyasecurity.com>
2020-06-20 22:49:57 -0400

    erased duplicate struct

--- pkg/fusionauth/Domain.go
+++ pkg/fusionauth/Domain.go
@@ -1706,11 +1706,9 @@ type LambdaConfiguration struct {
   AccessTokenPopulateId     string                    `json:"accessTokenPopulateId,omitempty"`
   IdTokenPopulateId         string                    `json:"idTokenPopulateId,omitempty"`
   Samlv2PopulateId          string                    `json:"samlv2PopulateId,omitempty"`
-}
-
-type LambdaConfiguration struct {
   ReconcileId               string                    `json:"reconcileId,omitempty"`
 }
+
 
 type ProviderLambdaConfiguration struct {
   ReconcileId               string                    `json:"reconcileId,omitempty"`
