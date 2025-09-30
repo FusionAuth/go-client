8dc1b285cb74adf546f9b4e626e2df75ae6af737
Brady Wied <brady.wied@fusionauth.io>
2025-09-30 16:32:16 -0600

    add skipUserRequirement to other repos

--- pkg/fusionauth/Domain.go
+++ pkg/fusionauth/Domain.go
@@ -7800,6 +7800,7 @@ type VerifyStartRequest struct {
 	ApplicationId        string                 `json:"applicationId,omitempty"`
 	LoginId              string                 `json:"loginId,omitempty"`
 	LoginIdType          string                 `json:"loginIdType,omitempty"`
+	SkipUserRequirement  bool                   `json:"skipUserRequirement"`
 	State                map[string]interface{} `json:"state,omitempty"`
 	VerificationStrategy VerificationStrategy   `json:"verificationStrategy,omitempty"`
 }
