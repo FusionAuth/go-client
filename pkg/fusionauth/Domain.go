94ddd13c11a470c023309f3fd1f54eabd308d4b5
Spencer Witt <3409780+spwitt@users.noreply.github.com>
2026-02-02 09:57:26 -0600

    add RegistrationConfiguration.completeRegistration ENG-3775

--- pkg/fusionauth/Domain.go
+++ pkg/fusionauth/Domain.go
@@ -295,17 +295,18 @@ type PasswordlessConfiguration struct {
 
 type RegistrationConfiguration struct {
 	Enableable
-	BirthDate          Requirable       `json:"birthDate,omitempty"`
-	ConfirmPassword    bool             `json:"confirmPassword"`
-	FirstName          Requirable       `json:"firstName,omitempty"`
-	FormId             string           `json:"formId,omitempty"`
-	FullName           Requirable       `json:"fullName,omitempty"`
-	LastName           Requirable       `json:"lastName,omitempty"`
-	LoginIdType        LoginIdType      `json:"loginIdType,omitempty"`
-	MiddleName         Requirable       `json:"middleName,omitempty"`
-	MobilePhone        Requirable       `json:"mobilePhone,omitempty"`
-	PreferredLanguages Requirable       `json:"preferredLanguages,omitempty"`
-	Type               RegistrationType `json:"type,omitempty"`
+	BirthDate            Requirable       `json:"birthDate,omitempty"`
+	CompleteRegistration bool             `json:"completeRegistration"`
+	ConfirmPassword      bool             `json:"confirmPassword"`
+	FirstName            Requirable       `json:"firstName,omitempty"`
+	FormId               string           `json:"formId,omitempty"`
+	FullName             Requirable       `json:"fullName,omitempty"`
+	LastName             Requirable       `json:"lastName,omitempty"`
+	LoginIdType          LoginIdType      `json:"loginIdType,omitempty"`
+	MiddleName           Requirable       `json:"middleName,omitempty"`
+	MobilePhone          Requirable       `json:"mobilePhone,omitempty"`
+	PreferredLanguages   Requirable       `json:"preferredLanguages,omitempty"`
+	Type                 RegistrationType `json:"type,omitempty"`
 }
 
 // This is separate from IdentityType.
